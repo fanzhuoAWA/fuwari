b1422b277911ec8d3849920e26abd8a8e9377010
fanzhuoAWA <fan_zhuo@outlook.com>
2025-09-06 15:36:22 +0800

    修改标题与副标题

--- src/config.ts
+++ src/config.ts
@@ -8,8 +8,8 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "Fanzhuo's Blog",
-	subtitle: "精选",
+	title: "Fanzhuo's Blog 精选",
+	subtitle: "(*^▽^*)",
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 195, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
